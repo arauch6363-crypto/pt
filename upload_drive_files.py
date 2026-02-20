cb97498a43ffee363444caca1cf6ba32836994cb
arauch6363-crypto <arauch6363@gmail.com>
2026-02-20 13:33:46 +0100

    Create upload_drive_files.py

---
+++ upload_drive_files.py
@@ -0,0 +1,29 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+# Load credentials
+credentials = service_account.Credentials.from_service_account_file(
+    'credentials.json',
+    scopes=['https://www.googleapis.com/auth/drive']
+)
+
+service = build('drive', 'v3', credentials=credentials)
+
+def upload_file(file_id, filename):
+    media = MediaFileUpload(filename)
+    service.files().update(fileId=file_id, media_body=media).execute()
+    print(f"Uploaded {filename} successfully!")
+
+upload_file('1tS8DsbCzeTUMj-DsB8A7bvM-XSCFa537', 'races.parquet')
+upload_file('1Tv20gDn7EWZMNIc0Nu9KRFJk-NM8OVgT', 'runners.parquet')
+upload_file('1-1dPegROQWIMhEHb8y2PezKfrLiE1x-6', 'reload_tracker.csv')
+upload_file('1DrevoYuGIcfq_4rYfz11H44pnANOnN1B', 'webTips.parquet')
+upload_file('1HG6V24mn9y_0uJH-13HIWxpcFwwE59ws', 'odds.parquet')
+upload_file('1-3OHnFfuM7qgJuq329gFa22jACuPGIeU', 'dividends.parquet')
+upload_file('1tS8DsbCzeTUMj-DsB8A7bvM-XSCFa537', 'races_tdy.parquet')
+upload_file('19g1udGm83tmRNTr10v598jtRox-bOWxq', 'runners_tdy.parquet')
+upload_file('1fqLUYNF1nfaCsw2K95fi-bWj4Jg0qdDo', 'webTips_tdy.parquet')
+upload_file('1Cx_juo4BT97gAqFysRQZyu8si_kb-zaU', 'odds_tdy.parquet')
+
+print("All files uploaded successfully!")
