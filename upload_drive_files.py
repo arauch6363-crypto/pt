a44302f99a6c68428850a67dc769c0a3d620389d
arauch6363-crypto <arauch6363@gmail.com>
2026-02-23 15:27:32 +0100

    Add download functionality for additional files

--- upload_drive_files.py
+++ upload_drive_files.py
@@ -31,5 +31,8 @@ upload_file('1Xet68Or274cfyX7To8yMbCNOCJN6kiWv', 'races_tdy.parquet')
 upload_file('19g1udGm83tmRNTr10v598jtRox-bOWxq', 'runners_tdy.parquet')
 upload_file('1fqLUYNF1nfaCsw2K95fi-bWj4Jg0qdDo', 'webTips_tdy.parquet')
 upload_file('1Cx_juo4BT97gAqFysRQZyu8si_kb-zaU', 'odds_tdy.parquet')
+download_file('1m3rf0xZxKqIQejHYXWLkL1Dx7NDzNbZD',   'df_with_ratings.parquet')
+download_file('1LG2GvIrv84popZCY9jKJ8YX0Yjq-NUGL',     'ratings_state.json')
+download_file('1O_36ELAU98GPvOBKs-ltdB_Z6bqkjK78', 'ratings_last_date.txt')
 
 print("All files uploaded successfully!")
