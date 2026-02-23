5a75accfc9f6bf041c3a60630d3e1f677ba21017
arauch6363-crypto <arauch6363@gmail.com>
2026-02-23 12:09:15 +0100

    Update file ID for downloading races_tdy.parquet

--- download_drive_files_today_update.py
+++ download_drive_files_today_update.py
@@ -18,7 +18,7 @@ def download_file(file_id, filename):
             status, done = downloader.next_chunk()
             print(f"Downloading {filename}: {int(status.progress() * 100)}%")
 
-download_file('1tS8DsbCzeTUMj-DsB8A7bvM-XSCFa537', 'races_tdy.parquet')
+download_file('1Xet68Or274cfyX7To8yMbCNOCJN6kiWv', 'races_tdy.parquet')
 download_file('19g1udGm83tmRNTr10v598jtRox-bOWxq', 'runners_tdy.parquet')
 download_file('1fqLUYNF1nfaCsw2K95fi-bWj4Jg0qdDo', 'webTips_tdy.parquet')
 download_file('1Cx_juo4BT97gAqFysRQZyu8si_kb-zaU', 'odds_tdy.parquet')
